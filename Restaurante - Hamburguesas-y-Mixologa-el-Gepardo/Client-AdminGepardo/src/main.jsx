744f873ce2f5572e852a00178a8cb0de8429844a
agarcia-2024043 <agarcia-2024043@kinal.edu.gt>
2026-05-03 21:45:49 -0600

    fix: Corregir las rutas de los styles

--- Restaurante - Hamburguesas-y-Mixologa-el-Gepardo/Client-AdminGepardo/src/main.jsx
+++ Restaurante - Hamburguesas-y-Mixologa-el-Gepardo/Client-AdminGepardo/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import "./style/index.css";
+import "./style/global.css";
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
