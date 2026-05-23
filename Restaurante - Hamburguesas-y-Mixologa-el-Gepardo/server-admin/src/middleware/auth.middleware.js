0844606b3dee1d13bbabc52b1d893d00db986de6
lcastro-2021276 <lcastro-2021276@kinal.edu.gt>
2026-05-22 20:31:11 -0600

    refactor: update auth.middleware.js  with and role verification

--- Restaurante - Hamburguesas-y-Mixologa-el-Gepardo/server-admin/src/middleware/auth.middleware.js
+++ Restaurante - Hamburguesas-y-Mixologa-el-Gepardo/server-admin/src/middleware/auth.middleware.js
@@ -1,36 +1,60 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
+
     const authHeader = req.headers["authorization"];
 
     if (!authHeader) {
-        return res.status(401).json({ message: "Token requerido" });
+        return res.status(401).json({
+            message: "Token requerido"
+        });
     }
 
     const token = authHeader.split(" ")[1];
 
     if (!token) {
-        return res.status(401).json({ message: "Token mal formado" });
+        return res.status(401).json({
+            message: "Token mal formado"
+        });
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET, {
-            issuer: "KinalSports",
-            audience: "KinalSports",
-        });
+
+        
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
         req.user = decoded;
+
         next();
+
     } catch (error) {
-        console.log("Error verificando token:", error.message); 
-        return res.status(403).json({ message: "Token inválido", error: error.message });
+
+        console.log(
+            "Error verificando token:",
+            error.message
+        );
+
+        return res.status(403).json({
+            message: "Token inválido",
+            error: error.message
+        });
     }
 };
 
 export const verifyRole = (role) => {
+
     return (req, res, next) => {
+
         if (!req.user || req.user.role !== role) {
-            return res.status(403).json({ message: "No autorizado" });
+
+            return res.status(403).json({
+                message: "No autorizado"
+            });
         }
+
         next();
     };
 };
